fa1850da720076600d5a37d8189b4368b416b88b
Esteban Mejia <estebandeveloper10@gmail.com>
2022-10-24 15:44:25 -0500

    Eliminacion de codigo inutil

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 // alert(`Nota: ¡Leer primero el REDME.md!  Éste es un proyecto de desarrollo web: E-commerce, que tiene implementaciones de HTML, CSS, JavaScript y Boostrap 5`);
 const userIcon = document.querySelector(`.user-icon-container`);
 const asideLogin = document.querySelector(`.aside-login-e`);
-const btnCart =  document.querySelector(`.button-cart`);
+const btnCart = document.querySelector(`.button-cart`);
 const closeLogin = document.querySelector(`.login-close`);
 const closeIconAlert = document.querySelector(`.close-img-container`);
 const alertLogin = document.querySelector(`.alert`);
@@ -12,21 +11,26 @@ closeLogin.addEventListener(`click`, closeAlideLogin);
 closeIconAlert.addEventListener(`click`, closeAlertLogin);
 
 function openAsideLogin() {
-    asideLogin.classList.remove(`inactive`);
-    
+  asideLogin.classList.remove(`inactive`);
 }
 
 function closeAlideLogin() {
-    asideLogin.classList.add(`inactive`);
+  asideLogin.classList.add(`inactive`);
 }
 
 function closeAlertLogin() {
-    alertLogin.classList.add(`inactive`);
+  alertLogin.classList.add(`inactive`);
 }
-
-
 //evento para el menu si iniciar seccion
-const tiendaSinSesion = document.querySelector("#tiendaSinSesion").addEventListener("click", ()=>  alert("Debes iniciar sesion primero para ver la lista de productos exclusivos"));
-const ProductoSinSeccion = document.querySelector("#ProductoSinSeccion").addEventListener("click", ()=>  alert("Debes iniciar sesion primero para ver Este producto"));
-
-
+const tiendaSinSesion = document
+  .querySelector("#tiendaSinSesion")
+  .addEventListener("click", () =>
+    alert(
+      "Debes iniciar sesion primero para ver la lista de productos exclusivos"
+    )
+  );
+const ProductoSinSeccion = document
+  .querySelector("#ProductoSinSeccion")
+  .addEventListener("click", () =>
+    alert("Debes iniciar sesion primero para ver Este producto")
+  );
