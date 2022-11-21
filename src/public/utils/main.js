7de0c39961b6616571b689d87d5b3287d16284ca
Esteban Mejia <estebandeveloper10@gmail.com>
2022-11-21 12:02:10 -0500

    Cambio necesario, arreglo de bugs

--- src/public/utils/main.js
+++ src/public/utils/main.js
@@ -1,12 +1,16 @@
 // alert(`Nota: ¡Leer primero el REDME.md!  Éste es un proyecto de desarrollo web: E-commerce, que tiene implementaciones de HTML, CSS, JavaScript y Boostrap 5`);
 const userIcon = document.querySelector(`.user-icon-container`);
+const userIconMibile = document.querySelector(`.user-icon-container-mobile`);
 const asideLogin = document.querySelector(`.aside-login-e`);
 const btnCart = document.querySelector(`.button-cart`);
 const closeLogin = document.querySelector(`.login-close`);
 const closeIconAlert = document.querySelector(`.close-img-container`);
 const alertLogin = document.querySelector(`.alert`);
 
+
+userIconMibile.addEventListener(`click`, openAsideLogin);
 userIcon.addEventListener(`click`, openAsideLogin);
+
 closeLogin.addEventListener(`click`, closeAlideLogin);
 closeIconAlert.addEventListener(`click`, closeAlertLogin);
 
