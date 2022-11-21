74c88b8b871d5bb3548d070ba549a01212d4eb1a
Esteban Mejia <estebandeveloper10@gmail.com>
2022-11-21 11:40:06 -0500

    Todo esta listo, solo faltaria la informaicon de contacto y de descubre

--- src/public/utils/main.js
+++ src/public/utils/main.js
@@ -16,6 +16,7 @@ function openAsideLogin() {
 
 function closeAlideLogin() {
   asideLogin.classList.add(`inactive`);
+  
 }
 
 function closeAlertLogin() {
@@ -36,54 +37,23 @@ const ProductoSinSeccion = document
   );
 
 
+  const navBarColapse = document.querySelector('#navbarNav')
+  const iconHamb = document.querySelector('.iconHamb')
+  
 
-  $(window).on("scroll", function() {
+  iconHamb.addEventListener('click', toggleNavList())
 
-    if ($(".navbar").offset().top > 40) {
-       $(".navbar").addClass("navbar-fixed");
-       $(".go-top").show();
- 
-    } else {
-       $(".navbar").removeClass("navbar-fixed");
-       $(".go-top").hide();
- 
-    }
- })
- 
- $(document).ready(function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-       itemSelector: '.portfolio-item'
-    });
- 
-    $('#portfolio-filters li').on('click', function() {
-       $("#portfolio-filters li").removeClass('filter-active');
-       $(this).addClass('filter-active');
- 
-       portfolioIsotope.isotope({
-          filter: $(this).data('filter')
-       });
-    });
- 
-    $('.popup-image').magnificPopup({
-       type: 'image',
-       closeOnContentClick: true,
-       gallery: {
-          enabled: true,
-          navigateByImgClick: true
-       }
-    });
- 
-    $("#navbarNav").on('show.bs.collapse', function() {
- 
-       $(".navbar").addClass("navbar-fixed");
- 
-       $('a.nav-link, a.btn-custom').click(function() {
-          $("#navbarNav").collapse('hide');
-       });
-    });
- 
- 
- });
+  const toggleNavList = () => {
+   navBarColapse.classList.toggle('inactive')
+  }
+
+
+
+
+
+
+
+
 
 
 
