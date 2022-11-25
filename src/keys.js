b0e6a2a0a33c15818dc67c3621bcead455c7cc24
Esteban Mejia <estebandeveloper10@gmail.com>
2022-11-25 12:10:45 -0500

    Configuracion para produccion

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,10 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_store'
+        host: process.env.DB_HOST ||'localhost',
+        user: process.env.DB_USER ||'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'database_store',
+        port: process.env.DB_PORT || 3306
     }
 };
 
